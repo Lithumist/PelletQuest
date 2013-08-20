2853934524164a18f0a9ff5c57cc892e6c4a2878
Pedro Custodio <magneticplum@live.co.uk>
2013-08-20 18:56:54 +0100

    First commit.

---
+++ Source/main.cpp
@@ -0,0 +1,14 @@
+/*
+	main.cpp
+
+	Entry point of the whole program, only invokes the engine.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello Fuckers!!\n";
+	return 0;
+}
